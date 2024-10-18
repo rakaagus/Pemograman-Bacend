4bf9d0463cd40903d5edfa40d17784412704b497
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-10-18 14:12:31 +0700

    delete directory oop & task

--- OOP/data-types.php
+++
@@ -1,5 +0,0 @@
-<?php
-$nama = "Edo Riansyah";  # string
-$umur = 34;              # integer
-$ipk = 3.5;              # float
-$isMarried = true;       # boolean
--- OOP/variable.php
+++
@@ -1,9 +0,0 @@
-<?php
-# membuat variable
-$nama = "Edo Riansyah";
-$jurusan = "Teknik Informatika";
-
-# memanggil variable
-echo $nama;
-echo "<br>";
-echo $jurusan;
