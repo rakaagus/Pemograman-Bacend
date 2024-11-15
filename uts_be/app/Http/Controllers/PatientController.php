ea370fe5f0862fbdaf7536a54610186785c3d6f2
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-11-15 15:50:30 +0700

    update count in response search patient

--- uts_be/app/Http/Controllers/PatientController.php
+++ uts_be/app/Http/Controllers/PatientController.php
@@ -219,6 +219,7 @@ class PatientController extends Controller
         return response()->json([
             'error' => false,
             'message' => 'Patients found',
+            'total' => $patients->count(),
             'data' => $patients,
         ], 200);
     }
