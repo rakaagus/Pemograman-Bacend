38aa8175c54bfdcad4cfea3051d6dd292cd934b0
Raka Agus Maulana <kakakunyukya@gmail.com>
2024-12-28 13:16:49 +0700

    Praktikum-13

--- task/controller/StudentController.js
+++ task/controller/StudentController.js
@@ -5,15 +5,33 @@ class StudentController {
     async index(req, res) {
         const students = await Student.all();
         
-        const data = {
-            message: "Manampilkan semua students",
-            data: students
+        if(students.length > 0){
+            const data = {
+                message: "Manampilkan semua students",
+                data: students
+            }
+    
+            res.json(data);
+        }else {
+            const data = {
+                message: "Tidak ada data student"
+            }
+    
+            res.json(data);
         }
-
-        res.json(data);
     }
 
     async store(req, res) {
+
+        const {nama, nim, email, jurusan} = req.body;
+
+        if(!nama || !nim || !email || !jurusan){
+            const data = {
+                message: `Semua Data harus dikirim`
+            }
+            res.json(data).status(422);
+        }
+
         const student = await Student.create(req.body);
 
         const data = {
@@ -23,18 +41,18 @@ class StudentController {
         res.json(data);
     }
 
-    update(req, res) {
+    async update(req, res) {
         const {id} = req.params;
-        const {nama} = req.body;
-        const student = students.find((s) => s.id == parseInt(id));
+        
+        const student = await Student.find(id);
 
         if(student){
 
-            student.nama = nama || student.nama;
+            const updateStudent = await Student.update(id, req.body);
 
             res.json(
                 {
-                    message: `Mengedit student id ${id}, nama ${nama}`,
+                    message: `Mengedit student id ${id}`,
                     data: student,
                 }
             ).status(200);
@@ -46,20 +64,40 @@ class StudentController {
         }
     }
 
-    destroy(req, res) {
+    async destroy(req, res) {
         const {id} = req.params;
-        const index = students.findIndex((s) => s.id === parseInt(id));
+        const student = Student.find(id);
         
-        if(index !== -1){
-            const deletedStudent = students.splice(index, 1);
+        if(student){
+            await Student.delete(id);
             res.json(
                 {
                     message: `Menghapus student id ${id}`,
-                    data: deletedStudent
                 }
             );
         }else {
             res.status(404).json({ message: 'Data student tidak ditemukan' });
+        }
+    }
+
+    async show(req, res){
+        const {id} = req.params;
+
+        const student = await Student.find(id);
+
+        if(student){
+            const data = {
+                message: "Menampilkan detail student",
+                data: student
+            };
+
+            res.json(data).status(200);
+        }else {
+            const data = {
+                message: "Student tidak ada",
+            };
+
+            res.json(data).status(404);
         }
     }
 }
--- task/routes/api.js
+++ task/routes/api.js
@@ -12,5 +12,6 @@ router.get('/students', StudentController.index);
 router.post('/students', StudentController.store);
 router.put('/students/:id', StudentController.update);
 router.delete('/students/:id', StudentController.destroy);
+router.get('/students/:id', StudentController.show);
 
 module.exports = router;
